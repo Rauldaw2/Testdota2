3ca65d0b040314befb510fa4bed09d4210101746
mjzato <mjzatojimenez@gmail.com>
2014-11-10 14:02:46 +0100

    added server.js with express

--- main/server/server.js
+++ main/server/server.js
@@ -2,12 +2,38 @@ var express = require('express');
 var debug = require('debug')('My-Dota2-Stats');
 var path = require('path');
 var fs = require('fs');
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+
 
 var app = express();
 
+
+app.use(favicon(__dirname + '/../client/favicon.ico'));
 app.use(express.static(path.join(__dirname, '/../client')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Custom error handler
+app.use(function(err, req, res, next) {
+	if (err) {
 
+		if (!err.statusCode || err.statusCode === 500) {
+			console.error(err);
+		}
+
+		var errorJson = {
+			err: err.err || err.code || 'server_error',
+			des: err.des || err.message || err.name || 'unknown'
+		};
+
+		res.json(err.statusCode || 500, errorJson);
+
+	} else {
+		next();
+	}
+});
 
 //
 // Routes
